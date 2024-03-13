f2383c86ca7cb987546eb3ad909b8009cdc83a36
Sharanya <datrangesharanya@gmail.com>
2024-03-14 02:37:07 +0530

    used  configureStore instead of createStore

--- redux-counter/src/redux/store.ts
+++ redux-counter/src/redux/store.ts
@@ -1,7 +1,15 @@
 
-import { createStore } from 'redux'
+
+import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './reducers/counterReducer'
 
-const store = createStore(counterReducer)
+const store=configureStore({
+    reducer:{
+        counter:counterReducer,
+    }
+});
+
+export type RootState=ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store
